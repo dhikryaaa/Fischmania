89e2045f7de9232b079725ae9f5d9dbd6317e45d
aldeen <71447171+aldeen1@users.noreply.github.com>
2024-12-15 12:31:32 +0700

    feat : added card class

--- edit-ikan.php
+++ edit-ikan.php
@@ -27,6 +27,22 @@ include("config.php");
             justify-content: center;
         }
 
+        .card {
+            flex: 1; 
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 60%;
+            gap: 10px;
+            max-width: 800px; 
+            height: auto;
+            background-color: #3F4149;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+        }
+
         table{
             border-collapse: collapse;
             width: 100%;
@@ -50,6 +66,9 @@ include("config.php");
     </style>
 </head>
 <body>
+    <div class="card">
+
+    </div>
     <header>
         <h3>Fish List</h3>
     </header>
--- edit-user.php
+++ edit-user.php
@@ -27,6 +27,22 @@ include("config.php");
             justify-content: center;
         }
 
+        .card {
+            flex: 1; 
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 60%;
+            gap: 10px;
+            max-width: 800px; 
+            height: auto;
+            background-color: #3F4149;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+        }
+
         table{
             border-collapse: collapse;
             width: 100%;
@@ -50,45 +66,47 @@ include("config.php");
     </style>
 </head>
 <body>
-<header>
-        <h3>User List</h3>
-    </header>
+    <div class="card">
 
-    <table>
-        <thead>
-        <tr>
-            <th>Email</th>
-            <th>Password</th>
-            <th>Usertype</th>
-        </tr>
-        </thead>
-        <tbody>
-            <?php
-            $sql = "SELECT * FROM users";
-
-            $query = mysqli_query($connect, $sql);
-
-            while($user = mysqli_fetch_array($query)){
-                $usertype = ($user['usertype'] == 1) ? "Admin" : "User";
-
-                echo "<tr>";
-
-                echo "<td>".$user['email']."</td>";
-                echo "<td>".$user['password']."</td>";
-                echo "<td>".$usertype."</td>";
-
-                echo "<td>";
-                echo "<a href='' class='button-blue'>Edit</a> | ";
-                echo "<a href='' class='button-red' onclick='return confirmDelete()'>Hapus</a>";
-                echo "</td>";
-
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <p style="text-align: center;">Total: <?php echo mysqli_num_rows($query) ?></p>
-
+        <header>
+            <h3>User List</h3>
+        </header>
+    
+        <table>
+            <thead>
+            <tr>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Usertype</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM users";
+    
+                $query = mysqli_query($connect, $sql);
+    
+                while($user = mysqli_fetch_array($query)){
+                    $usertype = ($user['usertype'] == 1) ? "Admin" : "User";
+    
+                    echo "<tr>";
+    
+                    echo "<td>".$user['email']."</td>";
+                    echo "<td>".$user['password']."</td>";
+                    echo "<td>".$usertype."</td>";
+    
+                    echo "<td>";
+                    echo "<a href='' class='button-blue'>Edit</a> | ";
+                    echo "<a href='' class='button-red' onclick='return confirmDelete()'>Hapus</a>";
+                    echo "</td>";
+    
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    
+        <p style="text-align: center;">Total: <?php echo mysqli_num_rows($query) ?></p>
+    </div>
 </body>
 </html>
